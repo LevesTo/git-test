beace37bfe71f5f0f6403cd19f1b10cc10e9705c
LevesTo <106469586+LevesTo@users.noreply.github.com>
2022-10-28 21:26:01 +0800

    pull commit

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git1111");
         System.out.println("hot-fix test");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
