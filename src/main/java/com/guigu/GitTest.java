26e1f0cfd8efa8bc87c7974f92d07a395115f6d3
yiyong <2253868687@qq.com>
2022-10-28 20:39:25 +0800

    hot-fix commit2

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git1");
         System.out.println("hello git1111");
+        System.out.println("hot-fix test");
     }
 }
